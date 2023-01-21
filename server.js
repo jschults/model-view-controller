1c44ec5849d725ed13b93d561b3aac9e203fb362
Julian Schultheis <jschults00@gmail.com>
2023-01-21 14:02:45 -0800

    adding code

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express'); 
+const path = require("path")
+const app = express();
+const PORT = 3001;
+
+  
+
+app.use(express.static('public')); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+res.sendFile(path.join(__dirname,'/index.html'));
+
+})
+
+
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
